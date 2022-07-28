d896b0ec44f3b256314692ac7da8da1e61998156
akash <akash.yadav@indwealth.in>
2022-07-28 18:19:08 +0530

    integrating react and react-dom as local dependency instead from unpk cdn

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 // Here we create a Pet Component consist of 1 nested div component
 // which in turn consist of one h1,h2 and h3 component.
 // This is just a blue-print of Pet component, just like a class in java.
