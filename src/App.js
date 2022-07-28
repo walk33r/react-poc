b5b0a2e43b516e8f60a08650f4febadf8b8f397d
akash <akash.yadav@indwealth.in>
2022-07-28 18:48:48 +0530

    translating App component code to JSX syntax

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import React from "react";
 import ReactDOM from "react-dom";
 
 import Pet from "./Pet";
-
-// Here we create a Pet Component consist of 1 nested div component
-// which in turn consist of one h1,h2 and h3 component.
-// This is just a blue-print of Pet component, just like a class in java.
 
 // Here we create an App Component consist of 1 nested div component
 // which in turn consist of one h1 component and 3 Pet component.
 // This is just a blue-print of App component, just like a class in java.
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mix",
-    }),
-  ]);
+  
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me"),
+  //   React.createElement(Pet, {
+  //     name: "Luna",
+  //     animal: "Dog",
+  //     breed: "Havanese",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Pepper",
+  //     animal: "Bird",
+  //     breed: "Cockatiel",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Doink",
+  //     animal: "Cat",
+  //     breed: "Mix",
+  //   }),
+  // ]);
+
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <Pet name='Luna' animal='Dog' breed='Havanese'/>
+      <Pet name='Pepper' animal='Bird' breed='Cockatiel'/>
+      <Pet name='Doink' animal='Cat' breed='Mix'/>
+    </div>
+  )
+
 };
 
 // On doing React.createElement App, we are creating an instance of App component
 // and passing it to ReactDOM to render.
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+// ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+ReactDOM.render(<App/>, document.getElementById("root"));
